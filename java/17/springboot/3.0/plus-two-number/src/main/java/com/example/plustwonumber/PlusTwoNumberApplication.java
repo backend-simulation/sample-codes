d38a09226284573b3f79ae78d92a215373961523
bron.kang <bron.kang@kakaoenterprise.com>
2023-01-01 02:59:43 +0900

    #1 feat: Initialize spring boot project

---
+++ java/17/springboot/3.0/plus-two-number/src/main/java/com/example/plustwonumber/PlusTwoNumberApplication.java
@@ -0,0 +1,13 @@
+package com.example.plustwonumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusTwoNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusTwoNumberApplication.class, args);
+    }
+
+}
